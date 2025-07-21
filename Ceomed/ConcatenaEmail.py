ef79da4e9d059a218a08ddc3a427a54e967abdc3
Mateus Bertani Dias <159319289+bertanj@users.noreply.github.com>
2025-07-21 16:19:44 -0300

    Update ConcatenaEmail.py

--- Ceomed/ConcatenaEmail.py
+++ Ceomed/ConcatenaEmail.py
@@ -4,40 +4,35 @@ import pandas as pd
 def concatenar_emails_excel(caminho_arquivo_excel, nome_coluna_email):
 
     try:
-        # Carrega o arquivo Excel em um DataFrame do pandas
-        # Use a variável caminho_arquivo_excel aqui
+        # Carrega o arquivo Excwel em um DataFrame
         df = pd.read_excel(caminho_arquivo_excel)
 
-        # Verifica se a coluna de e-mail existe no DataFrame
-        # Use a variável nome_coluna_email aqui
+        # Verifica se a coluna de e-mail existe no DF
         if nome_coluna_email not in df.columns:
             print(f"Erro: A coluna '{nome_coluna_email}' não foi encontrada no arquivo Excel.")
             return ""
 
         # Seleciona a coluna de e-mails, remove valores nulos e converte para string
-        # Use a variável nome_coluna_email aqui
         emails = df[nome_coluna_email].dropna().astype(str)
 
-        # Concatena os e-mails com ';'
+        # Concatena os emails com ';'
         emails_concatenados = ";".join(emails)
 
         return emails_concatenados
 
     except FileNotFoundError:
-        # Use a variável caminho_arquivo_excel aqui
         print(f"Erro: O arquivo '{caminho_arquivo_excel}' não foi encontrado.")
         return ""
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
         return ""
 
-    # Define o nome do seu arquivo Excel e o nome da coluna de e-mails
-    # Como o arquivo está na mesma pasta, basta o nome do arquivo.
-    caminho_do_seu_excel = 'clinicas_brasil.xlsx'
-    nome_da_coluna_de_email = 'EMAIL'
+    # Define o nome do Excel e o nome da coluna de email
+    caminho_excel = 'clinicas_brasil.xlsx'
+    coluna = 'EMAIL'
 
-    # Chama a função para concatenar os e-mails
-    lista_de_emails = concatenar_emails_excel(caminho_do_seu_excel, nome_da_coluna_de_email)
+    # Chama a função para concatenar os emails
+    lista_de_emails = concatenar_emails_excel(caminho_excel, coluna)
 
     # Imprime o resultado
     if lista_de_emails:
